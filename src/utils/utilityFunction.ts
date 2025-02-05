5baebaddf331e0c35e2ad86732d9f4db51c57897
Fernando Nikolas R <nikolasfernando2001@gmail.com>
2025-02-05 16:29:57 +0800

    feat: Add utility function
    A pure function that can be use anywhere

---
+++ src/utils/utilityFunction.ts
@@ -0,0 +1,10 @@
+/**
+ * 
+ * @param arr : any type of array
+ * @param times : how many replication you want to make
+ * @returns : n times number of array ex [1,2,3]*3 ==> [1,2,3,1,2,3,1,2,3] 
+ */
+export const duplicateArr = (arr:any[], times:number) : any[]=>
+    Array(times)
+        .fill([...arr])
+        .reduce((a, b) => a.concat(b));
