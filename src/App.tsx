a49b84f3d6f71f020005c607fb6ba161254902d5
Fernando Nikolas R <nikolasfernando2001@gmail.com>
2025-02-05 21:21:49 +0800

    feat: Insert about and Skills to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import About from "./section/About";
 import Hero from "./section/Hero";
 import Navbar from "./section/Navbar";
+import Skills from "./section/Skills";
 
 export default function App() {
   return <main className="max-w-6xl mx-auto">
     <h1 className="text-2xl text-white">
       <Navbar/>
       <Hero/>
+      <About/>
+      <Skills/>
     </h1>
   </main>
 }
