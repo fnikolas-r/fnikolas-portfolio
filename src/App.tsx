03e385b897498c11e978ab8213e1655c95bb05ad
Fernando Nikolas R <nikolasfernando2001@gmail.com>
2025-02-11 20:20:20 +0800

    build: Add React router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import About from "./section/About";
-import ContactMe from "./section/ContactMe";
-import EducationAndExperience from "./section/Experience";
-import Footer from "./section/Footer";
-import Hero from "./section/Hero";
-import Navbar from "./section/Navbar";
-import Projects from "./section/Projects";
-import Skills from "./section/Skills";
-import { ToastContainer } from 'react-toastify';
+import { Routes, Route } from "react-router";
+import Home from "./pages/home";
+
 
 
 export default function App() {
   const queryClient = new QueryClient()
   return (
     <QueryClientProvider client={queryClient}>
-      <main className="max-w-6xl mx-auto text-white">
-          <Navbar />
-          <Hero />
-          <About />
-          <Skills />
-          <Projects />
-          <EducationAndExperience />
-          <ContactMe />
-          <Footer />
-          <ToastContainer/>
-      </main>
+      <Routes>
+        <Route index element={<Home/>}/>
+      </Routes>
     </QueryClientProvider>
   );
 }
